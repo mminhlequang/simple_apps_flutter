cfb48511ab09b32154da7602c7758560a751d233
Minh Minh <mminh.lequang@gmail.com>
2024-06-13 19:48:10 +0700

    -

---
+++ sources/LOGO_QUIZ/android/app/src/main/kotlin/com/egylab/bskal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.sflagquizs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sources/LOGO_QUIZ/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'AdHelper.dart';
+import 'dart:async';
+
+import 'HomeScreen.dart'; // <- Import the package
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // MobileAds.instance.initialize();
+
+  // AdHelper.loadOpenAd().then((_) {
+  //   AdHelper.showOpenAdIfAvailable();
+  // });
+  // OneSignal.shared.setAppId('2b8b183c-9b22-416e-b2d1-f9bfe907afc7');
+
+  // // Optional settings for handling notifications and in-app messages
+  // OneSignal.shared
+  //     .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+  //   print('Tapped: ${result.notification.jsonRepresentation()}');
+  // });
+
+  // OneSignal.shared.setInAppMessageClickedHandler((OSInAppMessageAction action) {
+  //   // Handle in-app message click event
+  // });
+  // Initialize OneSignal
+
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        AdHelper.handleClick();
+      },
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(),
+        },
+      ),
+    );
+  }
+}
