871fd0a4bbee140e42b6b122bd7fe442fa8aae62
Minh Minh <mminh.lequang@gmail.com>
2024-07-23 10:44:56 +0700

    -

--- apps/pictalk_languages/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/pictalk_languages/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
-import connectivity_plus
 import device_info_plus
 import firebase_core
 import flutter_tts
@@ -19,7 +18,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
